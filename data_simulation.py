eb5ac423eeb3ba574fc5816ed338730d6f5082d0
keith <keith@kotyk.io>
2020-11-25 16:26:27 -0600

    Changing writes to borealis files
    Changed to writes to use borealis files instead. This will be more
    consistent when trying to write out all files types.

--- data_simulation.py
+++ data_simulation.py
@@ -33,7 +33,7 @@ def plotting_acfs(averaging_period_acfs, model, lags, t, amplitude):
     lag_nums = lags[:,1] - lags[:,0]
     num_averages = averaging_period_acfs.shape[1]
     model = model[lag_nums]
-    t = t[lag_nums]
+    t = t[0,lag_nums]
 
     fig = pyplot.figure(figsize=(12,6))
     ax = fig.add_subplot(121)
@@ -74,49 +74,40 @@ def create_output_params(transmit_freqs, white_noise_level, sample_separation, p
     now = datetime.datetime.utcnow()
     sequence_time = pulses[-1] * (fundamental_lag_spacing * 1e-6) + (num_ranges * sample_separation * 1e-6)
     sequence_times = [now + datetime.timedelta(seconds=(i * sequence_time)) for i in range(num_records)]
+    sequence_times = [dt.timestamp() for dt in sequence_times]
 
     common_fields = {}
-
-    common_fields['radar.revision.major'] = np.int8(0)
-    common_fields['radar.revision.minor'] = np.int8(0)
-    common_fields['origin.code'] = np.int8(0)
-    common_fields['origin.time'] = ' '
-    common_fields['origin.command'] = 'python3 data_simulation.py'
-    common_fields['cp'] = np.int16(0)
-    common_fields['stid'] = np.int16(6)
-    common_fields['txpow'] = np.int16(0)
-    common_fields['nave'] = np.int16(num_averages)
-    common_fields['atten'] = np.int16(0)
-    common_fields['lagfr'] = np.int16(first_range * sample_separation)
-    common_fields['smsep'] = np.int16(sample_separation)
-    common_fields['ercod'] = np.int16(0)
-    common_fields['stat.agc'] = np.int16(0)
-    common_fields['stat.lopwr'] = np.int16(0)
-    common_fields['noise.search'] = np.float32(0)
-    common_fields['noise.mean'] = np.float32(0)
-    common_fields['channel'] = np.int16(0)
-    common_fields['bmnum'] = np.int16(0)
-    common_fields['bmazm'] = np.float32(0)
-    common_fields['scan'] = np.int16(0)
-    common_fields['offset'] = np.int16(0)
-    common_fields['rxrise'] = np.int16(0)
-    common_fields['combf'] = "Simulated data"
-
-    integer, decimal = divmod(sequence_time * num_averages, 1)
-    common_fields['intt.sc'] = np.int16(integer)
-    common_fields['intt.us'] = np.int32(decimal)
-
-    common_fields['txpl'] = np.int16(sample_separation)
-    common_fields['mpinc'] = np.int16(fundamental_lag_spacing)
-    common_fields['mppul'] = np.int16(pulses.shape[0])
-    common_fields['mplgs'] = np.int16(lags.shape[0])
-    common_fields['nrang'] = np.int16(num_ranges)
-    common_fields['frang'] = np.int16(first_range * range_separation)
-    common_fields['rsep'] = np.int16(range_separation)
-    common_fields['xcf'] = np.int16(0)
-    common_fields['mxpwr'] = np.int32(0)
-    common_fields['lvmax'] = np.int32(0)
-    common_fields['ptab'] = pulses.astype(np.int16)
+    common_fields['averaging_method'] = np.array(['mean'])[0]
+    common_fields['beam_azms'] = np.zeros(num_records, dtype=np.float64)
+    common_fields['beam_nums'] = np.zeros(num_records, dtype=np.uint32)
+    common_fields['blanked_samples'] = np.zeros(num_records, dtype=np.uint32)
+    common_fields['borealis_git_hash'] = np.array(['v0.5'])[0]
+    common_fields['data_normalization_factor'] = np.float64(1.0)
+    common_fields['experiment_comment'] = np.array(["Simulated data"])[0]
+    common_fields['experiment_id']  = np.int64(0)
+    common_fields['experiment_name'] = np.array(["data_simulation"])[0]
+    common_fields['first_range_rtt'] = np.float32(first_range * sample_separation)
+    common_fields['int_time'] = np.ones(num_records, dtype=np.float32) * sequence_time
+    common_fields['intf_antenna_count'] = np.uint32(4)
+    common_fields['main_antenna_count'] = np.uint32(16)
+    common_fields['noise_at_freq'] = np.zeros((num_records, num_averages))
+    common_fields['num_sequences'] = np.ones(num_records, dtype=np.int64) * num_averages
+    common_fields['num_slices'] = np.ones(num_records, dtype=np.int64) * transmit_freqs.shape[0]
+    common_fields['pulses'] = pulses.astype(np.uint32)
+    common_fields['range_sep'] = np.float32(range_separation)
+    common_fields['rx_sample_rate'] = np.float64(1 / sample_separation)
+    common_fields['samples_data_type'] = np.array(['complex float'])[0]
+    common_fields['scan_start_marker'] = np.zeros(num_records, dtype=np.bool)
+    common_fields['scheduling_mode'] = np.array(['None'])[0]
+    common_fields['slice_comment'] = np.array(["Simulated data"])[0]
+    common_fields['num_blanked_samples'] = np.zeros(num_records, np.uint32)
+    common_fields['num_beams'] = np.ones(num_records, dtype=np.uint32)
+    common_fields['first_range'] = np.float32(first_range * range_separation)
+    common_fields['slice_interfacing'] = np.array(['']*num_records)
+    common_fields['sqn_timestamps'] = np.array(sequence_times)
+    common_fields['station'] = np.array(['sas'])[0]
+    common_fields['tau_spacing'] = np.uint32(fundamental_lag_spacing)
+    common_fields['tx_pulse_len'] = np.uint32(sample_separation)
 
     shape = list(lags.shape)
     shape[0] += 1
@@ -124,42 +115,20 @@ def create_output_params(transmit_freqs, white_noise_level, sample_separation, p
     new_lags = np.zeros(shape)
     new_lags[:-1,:] = lags
     new_lags[-1,:] = pulses[-1]
-    common_fields['ltab'] = new_lags.astype(np.int16)
+    common_fields['lags'] = lags.astype(np.uint32)
 
 
     for i in range(transmit_freqs.shape[0]):
-        rawacf_records = []
-        for j in range(num_records):
-            record = copy.deepcopy(common_fields)
-            record['time.yr'] = np.int16(sequence_times[j].year)
-            record['time.mo'] = np.int16(sequence_times[j].month)
-            record['time.dy'] = np.int16(sequence_times[j].day)
-            record['time.hr'] = np.int16(sequence_times[j].hour)
-            record['time.mt'] = np.int16(sequence_times[j].minute)
-            record['time.sc'] = np.int16(sequence_times[j].second)
-            record['time.us'] = np.int32(sequence_times[j].microsecond)
-            record['tfreq'] = np.int16(transmit_freqs[i])
+        rawacf_data = copy.deepcopy(common_fields)
+        rawacf_data['slice_id'] = np.uint32(i)
+        rawacf_data['freq'] = np.uint32(transmit_freqs[i])
+        rawacf_data['correlation_descriptors'] = np.array(['num_records', 'num_beams', 'num_ranges', 'num_lags'])
+        rawacf_data['main_acfs'] = sim_acfs[:,i].astype(np.complex64)
+        rawacf_data['xcfs'] = np.zeros(sim_acfs[:,i].shape, dtype=np.complex64)
+        rawacf_data['intf_acfs'] = np.zeros(sim_acfs[:,i].shape, dtype=np.complex64)
 
-
-            rawacf_record = copy.deepcopy(record)
-            rawacf_record['rawacf.revision.major'] = np.int32(0)
-            rawacf_record['rawacf.revision.minor'] = np.int32(0)
-            rawacf_record['thr'] = np.float32(0)
-            rawacf_record['pwr0'] = pwr0[j].astype(np.float32)
-            rawacf_record['acfd'] = acfd[j].astype(np.float32)
-            rawacf_record['slist'] = np.arange(num_ranges, dtype=np.int16)
-
-            records.append(record)
-
-        filename = "simulated_{}.rawacf".format(i)
-        writer = pydarnio.SDarnWrite(records)
-        writer.write_rawacf(filename)
-
-
-
-
-
-
+        filename = "simulated_{}.rawacf.hdf5".format(i)
+        writer = pydarnio.BorealisWrite(filename, rawacf_data, 'rawacf', 'array')
 
 
 def main():
@@ -224,7 +193,6 @@ def main():
 
     cov_mat = rho.reshape(new_shape1).transpose(new_axis).reshape(new_shape2)
     cov_mat /= 2.0
-    print(cov_mat.shape)
 
     # Signs need to be flipped for every second value on every second diagonal to properly account
     # for the sign changes.
@@ -241,25 +209,6 @@ def main():
         signs[rows_idx,cols_idx] *= -1.0
 
     cov_mat *= signs[np.newaxis, np.newaxis, :, :]
-    #plotting_cov_mat(cov_mat[-1,-1])
-
-
-    # tst_cov = amplitude * np.diagflat(np.ones(2*highest_lag))
-    # for j in range(0,2*highest_lag,2):
-    #     for k in range(j+2,2*highest_lag,2):
-    #         rho = acf_model[(k-j)//2]
-    #         #upper diagonal blocks
-    #         tst_cov[j,k] = rho.real
-    #         tst_cov[j,k+1] = -1 * rho.imag
-    #         tst_cov[j+1,k] = rho.imag
-    #         tst_cov[j+1,k+1] = rho.real
-    #         #lower diagonal blocks
-    #         tst_cov[k,j] = rho.real
-    #         tst_cov[k,j+1] = rho.imag
-    #         tst_cov[k+1,j] =  -1 * rho.imag
-    #         tst_cov[k+1,j+1] = rho.real
-
-    # tst_cov /= 2
 
     # create the covariance matrix of the noise.
     noise_cov = np.diagflat(np.ones(2 * highest_lag)) * white_noise_level
@@ -307,8 +256,10 @@ def main():
     all_acfs = np.einsum('...ik,...kj->...ij', samples, samples_H)
 
     sim_acfs = all_acfs[...,lags[:,0],lags[:,1]]
-    sim_acfs = np.mean(sim_acfs, axis=2)
+    sim_acfs = np.mean(sim_acfs, axis=3)
 
+    # add axis for num beams
+    sim_acfs = sim_acfs[:,:,np.newaxis,:,:]
     pwr0 = np.abs(sim_acfs)[...,0]
 
     acfd = np.zeros(sim_acfs.shape + (2,))
@@ -325,20 +276,9 @@ def main():
         idx = pulses * int(fundamental_lag_spacing / sample_separation) + i + first_range
         iq_samps[...,idx] += samples_temp[...,i,:]
 
-    fig = pyplot.figure()
-    ax = fig.add_subplot(111)
-    tmp=ax.plot(iq_samps[0,0,:,:].T.real)
-    pyplot.show()
-    #print(iq_samps.shape)
-    #iq_samps = np.einsum('ijklm->ijlmk', iq_samps)
-    #iq_samps = iq_samps.reshape(iq_samps.shape[:3] + (-1,))
-    #print(iq_samps.shape)
 
     create_output_params(transmit_freqs, white_noise_level, sample_separation, pulses, lags,
                         first_range, range_separation, fundamental_lag_spacing, sim_acfs)
-
-    #plotting_acfs(sim_acfs[0,0], acf_model, lags, t, amplitude)
-    #plotting_cov_mat(cov_mat)
 
 
 if __name__ == "__main__":
